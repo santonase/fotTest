f2befa8fae192c13f63029de923cbfd96065ca43
Sviatoslav Binovskyi <santonase@gmail.com>
2022-08-11 19:17:56 +0300

    practice Array

--- test/test/ViewController.swift
+++ test/test/ViewController.swift
@@ -9,15 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var a = 1
-    var b = "2"
-    var c: Float = 3
-    var sum: Double = 0.0
+    let newStudent1 = "Joahn"
+    let newStudent2 = "Nick"
+    
+    var students = ["bob", "alice", "jack", "kate", "rob"]
+    var studenrs2 = ["bob-bob", "alice-alice"]
+    var group: [String] = []
+    var newGroup: [String] = []
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        print(a + Int(b)! + Int(c))
+        
+        print(students)
+        students.append(newStudent1)
+        print(students)
+        students.insert(newStudent2, at: 0)
+        print(students)
+        students.removeFirst()
+        print(students)
+        students.remove(at: 4)
+        print(students)
+        
+        group = students + studenrs2
+        print(group)
+        print(group.first)
+        print(group.last)
+        print(group[1])
+        print(group[2])
+        
+        print("Array Group has: \(group.count) elements")
+        print("Is array empty?: \(group.isEmpty)")
+        
+        newGroup = group
+        print(newGroup)
+        group.removeAll()
+        print(group.isEmpty)
+        
     }
+    
     
    
     
